dff42e4d97c7541f1fdd6e394da3b8904bddbe93
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-04-01 21:48:17 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -54,6 +54,9 @@ class Waiter
       if waiter.yrs_experience > most_experienced
         most_experienced = waiter.yrs_experience
         waiter.meals.each {|meal| most_experienced_tips << meal.tip}
+      elsif least_experienced == 0
+        least_experienced = waiter.yrs_experience
+        waiter.meals.each {|meal| least_experienced_tips << meal.tip}
       elsif waiter.yrs_experience < least_experienced
         least_experienced = waiter.yrs_experience
         waiter.meals.each {|meal| least_experienced_tips << meal.tip}
